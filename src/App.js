723eca077a1c4925d2af30beb22da2d841bc21f4
Vedant <vedant28j@gmail.com>
2025-07-02 06:42:41 +0000

    Comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 
 const App=()=>{
-  const name=null;
+  // const name=null;
   // const isNameShowing=false;
-  const isNameShowing=false;
+  // const isNameShowing=false;
   return (
     <div className="App">
-      <h1>Hello {isNameShowing ? name : "Buddy"}!</h1>
+      <h1>Hello</h1>
+      {/* <h1>Hello {isNameShowing ? name : "Buddy"}!</h1>
       {name ? (
         <>
           test
@@ -16,7 +17,7 @@ const App=()=>{
           <h1>Test</h1>
           <h2>There is no name</h2>
         </>
-      )}
+      )} */}
     </div>
   );
 }

5cb855afc0be3d74b4fa73f932ec42b6ecc181ed
Vedant <vedant28j@gmail.com>
2025-07-02 06:47:09 +0000

    React Component: a const outside the App component which can be used multiple times inside App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 
+const Person =()=>{
+  return (
+    <>
+    <h1>First Name: Vedant</h1>
+    <h2>Last Name: Jalan</h2>
+    <h2>Age: 21</h2>
+    </>
+  )
+}
+
 const App=()=>{
   // const name=null;
   // const isNameShowing=false;
   // const isNameShowing=false;
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+      <Person />
       {/* <h1>Hello {isNameShowing ? name : "Buddy"}!</h1>
       {name ? (
         <>

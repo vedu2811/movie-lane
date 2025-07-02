dc5eb70ac66c65c0ee6e80e17fe43a88097c6f5b
Vedant <vedant28j@gmail.com>
2025-07-02 06:53:37 +0000

    Comments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 
-const Person =(props)=>{
-  return (
-    <>
-    <h1>First Name: {props.name}</h1>
-    <h2>Last Name: {props.lastName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
+// const Person =(props)=>{
+//   return (
+//     <>
+//     <h1>First Name: {props.name}</h1>
+//     <h2>Last Name: {props.lastName}</h2>
+//     <h2>Age: {props.age}</h2>
+//     </>
+//   )
+// }
 
 const App=()=>{
   // const name=null;
@@ -16,8 +16,8 @@ const App=()=>{
   // const isNameShowing=false;
   return (
     <div className="App">
-      <Person name="Vedant" lastName="Jalan" age={21}/>
-      <Person name="John" lastName="Doe" age={30}/>
+      {/* <Person name="Vedant" lastName="Jalan" age={21}/>
+      <Person name="John" lastName="Doe" age={30}/> */}
       {/* <h1>Hello {isNameShowing ? name : "Buddy"}!</h1>
       {name ? (
         <>

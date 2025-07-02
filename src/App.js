5d03c007b842b7a4175abaf1f13a68d9d0145425
Vedant <vedant28j@gmail.com>
2025-07-02 16:21:54 +0000

    Implementing the API_URL feature in the movie-lane app IMPORTANT COMMIT MESSAGE

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import {useEffect} from 'react';
 
 // Omdb API Key
 // fcea5672
 
+const API_URL = 'https://www.omdbapi.com/?apikey=fcea5672';
+
 const App = () => {
+    const searchMovies =async(title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data=await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Harry Potter');
+    })
     return(
         <h1>App</h1>
     );

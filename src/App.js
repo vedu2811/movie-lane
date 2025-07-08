e51ee2b60275bf063e62d50bac913317f844cbd9
Vedant <vedant28j@gmail.com>
2025-07-08 16:37:19 +0000

    Using useState hook with searchTerm to function the search box

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ const movie1={
 
 const App = () => {
     const[movies,setMovies]=useState([]);
+    const [searchTerm,setSearchTerm]=useState('');
+
+
     const searchMovies =async(title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data=await response.json();
@@ -35,8 +38,8 @@ const App = () => {
             <div className="search">
                 <input 
                     placeholder="Search for movies"
-                    value="Harry Potter"
-                    onChange={() => {}}
+                    value={searchTerm}
+                    onChange={(e) =>setSearchTerm(e.target.value)}
                 />
                 <img 
                     src={searchIcon}

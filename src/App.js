7f4b9929b28f0750df2498f09763c82702caa4fe
Vedant <vedant28j@gmail.com>
2025-07-08 16:04:47 +0000

    Completing one container of a movie

--- src/App.js
+++ src/App.js
@@ -49,7 +49,11 @@ const App = () => {
                         <p>{movie1.Year}</p>
                     </div>
                     <div>
-                        <img src={movie1.Poster !== 'N/A' ? movie1.poster : 'https://via.placeholder.com/400'} alt={movie1.title}/>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.title}/>
+                    </div>
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
                     </div>
                 </div>
             </div>
